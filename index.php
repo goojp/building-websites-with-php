4ed11fba18134d5bd6508a5f9f39f91f25c1afd7
goojp <goojp@outlook.com>
2017-02-27 03:19:04 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+require 'vendor/autoload.php';
+
+date_default_timezone_set('Asia/Shanghai');
+
+// use Monolog\Logger;
+// use Monolog\Handler\StreamHandler;
+
+// $log = new Logger('name');
+// $log->pushHandler(new StreamHandler('app.txt', Monolog\Logger::WARNING));
+// $log->addWarning('Foo');
+// $log->addWarning('Hello');
+
+
+
+$app = new Slim\Slim( array(
+    'view' => new \Slim\Views\Twig()
+    ));
+
+$view = $app->view();
+
+$view->parserOptions = array('debug' => true);
+$view->parserExtensions = array(
+    new \Slim\Views\TwigExtension(),
+    );
+
+$app->get('/', function () use($app) {
+    $app->render('about.twig');
+    
+});
+ 
+$app->get('/contact', function () use($app) {
+   $app->render('contact.twig');
+});
+
+
+
+
+$app->run();
